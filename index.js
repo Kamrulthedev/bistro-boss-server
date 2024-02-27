5bff1c0fbf301bef8d3f3d00ddf2f8e8d066d071
Kamrul Hassan <kamrulthedev@gmail.com>
2024-02-27 14:25:09 +0600

    user-insert-commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
 const port = process.env.PORT || 5000;
 
 
@@ -28,7 +28,26 @@ async function run() {
     const menuCollection = client.db("BistroDB").collection("menu");
     const reviewsCollection = client.db("BistroDB").collection("reviews");
     const cardsCollection = client.db("BistroDB").collection("cards");
+    const userCollection = client.db("BistroDB").collection("users");
 
+
+
+
+    // users related api 
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      // insert email if user doesnt exists :
+      // you can do this many (1. email unique , 2. upsert 3. simple checking)
+      const query = { email: user.email }
+      const existingUser = await userCollection.findOne(query)
+      if (existingUser) {
+        return res.send({ message: 'user alredy exists', insertedId: null })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    
 
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
